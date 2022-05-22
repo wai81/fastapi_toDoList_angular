dd639135c7d3022d0bdbebad30a07ecf626999a8
wai81 <50743966+wai81@users.noreply.github.com>
2022-05-22 23:39:39 +0300

    added delete todo method

--- angular/src/app/todo-list/todo-list.service.ts
+++ angular/src/app/todo-list/todo-list.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/Todo';
@@ -22,6 +22,14 @@ export class TodoListService {
 
   find(id: number): Observable<Todo>{
     return this.http.get<Todo>(`${this.resourceUrl}/${id}`);
+  }
+
+  findAll(): Observable<Todo[]>{
+    return this.http.get<Todo[]>(this.resourceUrl);
+  }
+
+  delete(id: number | undefined): Observable<HttpResponse<any>>{
+    return this.http.delete<any>(`${this.resourceUrl}/${id}`);
   }
 
   private convert(todo: Todo): Todo {
