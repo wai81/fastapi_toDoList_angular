b07d209eadd57dc469b5b7c25738ba981e7d2294
wai81 <50743966+wai81@users.noreply.github.com>
2022-05-22 23:29:27 +0300

    added method create todo

--- angular/src/app/todo-list/todo-list.service.ts
+++ angular/src/app/todo-list/todo-list.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
 
 
 @Injectable()
@@ -7,4 +9,14 @@ export class TodoListService {
   private resourceUrl = 'http://localhost:8000/todos';
 
   constructor(private http: HttpClient) { }
+
+  create(todo: Todo): Observable<Todo>{
+    const copy = this.convert(todo);
+    return this.http.post<Todo>(this.resourceUrl, copy);
+  }
+
+  private convert(todo: Todo): Todo {
+    const copy: Todo = Object.assign({}, todo);
+    return copy;
+  }
 }
